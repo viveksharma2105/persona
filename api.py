64e481dbba409993870e8f7580c269e2b8c3172b
Vivek Sharma <viveksharma02005@gmail.com>
2026-02-05 10:49:01 +0530

    update:cors

--- api.py
+++ api.py
@@ -13,7 +13,7 @@ app = FastAPI()
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:5174", "http://localhost:3000"],
+    allow_origins=["http://localhost:5173", "http://localhost:5174", "http://localhost:3000","https://persona.viveksh.me"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
